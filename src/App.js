ce88925436b12830642deca6bf18106ed776a091
Natalikud <student_fullstack@mail.ru>
2024-03-26 14:11:33 +0800

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './components/steps/css/style.css';
+import StepCounter from './components/steps/js/stepCounter';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StepCounter />
     </div>
   );
 }
